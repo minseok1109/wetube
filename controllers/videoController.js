cfa091f133b16cc2e367a4ad1b22767d05a9b791
minseok1109 <park485201@naver.com>
2020-09-19 00:54:58 +0900

    making change password

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -53,6 +53,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
+    console.log('video: ', video);
     res.render('videoDetail', { pageTitle: `${video.title}`, video });
   } catch (error) {
     res.redirect(routes.home);
