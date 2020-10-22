1c46494481aab9ba65e98552ec6b0ca5176b9bba
minseok1109 <park485201@naver.com>
2020-10-22 22:54:55 +0900

    add facebook login

---
+++ src/FBpassport.js
@@ -0,0 +1,25 @@
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import User from './models/User';
+import { facebookLoginCallback } from './controllers/userController';
+import routes from './routes';
+import dotenv from 'dotenv';
+dotenv.config();
+
+passport.use(User.createStrategy());
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FB_ID,
+      clientSecret: process.env.FB_SECRET,
+      callbackURL: `https://calm-scrubland-53159.herokuapp.com/${routes.facebookCallback}`,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+      scope: ['public_profile', 'email'],
+    },
+    facebookLoginCallback
+  )
+);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
--- src/passport.js
+++ src/passport.js
@@ -1,12 +1,7 @@
 import passport from 'passport';
 import GithubStrategy from 'passport-github';
-// import FacebookStrategy from 'passport-facebook';
 import User from './models/User';
-import {
-  // facebookLoginCallback,
-  githubLoginCallback,
-} from './controllers/userController';
-// import routes from './routes'
+import { githubLoginCallback } from './controllers/userController';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -25,19 +20,6 @@ passport.use(
     githubLoginCallback
   )
 );
-
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       clientID: process.env.FB_ID,
-//       clientSecret: process.env.FB_SECRET,
-//       callbackURL: `https://ordinary-robin-4.serverless.social${routes.facebookCallback}`,
-//       profileFields: ['id', 'displayName', 'photos', 'email'],
-//       scope: ['public_profile', 'email'],
-//     },
-//     facebookLoginCallback
-//   )
-// );
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
